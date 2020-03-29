70a70e98c3d12391139060fa297cb2a1eeb360b3
nanter frechat <nanter1986@gmail.com>
2020-03-29 11:23:29 +0300

    function ideas

--- first.py
+++ first.py
@@ -1,3 +1,11 @@
 #create world database if it doesnt exist
+
+#function for building area
+
+#function for building blocks of buildings
+
+#function for building buildings
+
 #show enviroment
+
 #show hud
