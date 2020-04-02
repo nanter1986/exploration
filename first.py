ff1ab3a4f7954094c78b753466a25f8f8e2175f8
nanter frechat <nanter1986@gmail.com>
2020-04-02 17:16:40 +0300

    storyline switch & build area

--- first.py
+++ first.py
@@ -1,9 +1,13 @@
 #create world database if it doesnt exist
 world=[]
 #storyline switches
-
+switch=False
 #function for building area
-
+def build_area(size):
+    area=""
+    for(i=0;i<size;i++):
+        area+="|"
+    return area
 #function for building blocks of buildings
 
 #function for building buildings
